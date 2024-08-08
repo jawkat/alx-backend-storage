b75481f8f662d279d7b061c9752b4b8df88051dd
jawkat <kad.joliano@gmail.com>
2024-08-08 21:53:29 +0100

    try

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,13 @@
 -- SQL script that creates a trigger that resets valid_email
 -- only when the email has been changed.
+DELIMITER $$
 
 CREATE TRIGGER email_reset BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-        UPDATE users SET NEW.valid_email = 0
+        UPDATE users SET NEW.valid_email = 0;
     END IF;
-END;
+END $$
+
+DELIMITER;
