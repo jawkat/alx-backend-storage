0440476115d2696795c912f961a9440220efc748
jawkat <kad.joliano@gmail.com>
2024-08-13 16:33:29 +0100

    task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" comments """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """_summary_
+
+    Args:
+        mongo_collection (_type_): _description_
+        topic (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return mongo_collection.find({"topic": topic})
--- 0x01-NoSQL/test.py
+++ 0x01-NoSQL/test.py
@@ -2,28 +2,29 @@
 
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
 
-def update_topics(mongo_collection, name, topics):
-    mongo_collection.update_many(
-        {"name": name},
-        {"$set":{"topics": topics}}
-        )
+
+
+def schools_by_topic(mongo_collection, topic):
+    docs = mongo_collection.find({"topic": topic})
+    return docs
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
-    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
 
-    schools = list_all(school_collection)
+    j_schools = [
+        { 'name': "Holberton school", 'topics': ["Algo", "C", "Python", "React"]},
+        { 'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
+        { 'name': "UCLA", 'topics': ["C", "Python"]},
+        { 'name': "UCSD", 'topics': ["Cassandra"]},
+        { 'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
+    ]
+    for j_school in j_schools:
+        insert_school(school_collection, **j_school)
+
+    schools = schools_by_topic(school_collection, "Python")
     for school in schools:
         print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
-    update_topics(school_collection, "Holberton school", ["iOS"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
-    # for doc in docs:
-    #     print(f"[{doc.get('_id')}] {doc.get('name')}")
